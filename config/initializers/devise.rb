cecdcf417380bf556785cc87685a0fe3e6e23c7a
AnkitaJaiswal <ankita.jaiswal@quantiguous.com>
2016-02-24 17:41:19 +0530

    initial commit

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -193,6 +193,9 @@ Devise.setup do |config|
   # change their passwords.
   config.reset_password_within = 6.hours
 
+  config.reset_password_keys = [ :username ]
+  config.confirmation_keys = [ :username ]
+
   # When set to false, does not sign a user in automatically after their password is
   # reset. Defaults to true, so a user is signed in automatically after a reset.
   # config.sign_in_after_reset_password = true
