6338eb42c24382d85a8d795c0bde9ba01e0ecec4
AnkitaJaiswal <ankita.jaiswal@quantiguous.com>
2016-03-04 14:40:13 +0530

    fixed the sign_out

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,24 +4,5 @@ class ApplicationController < ActionController::Base
   #before_action :authenticate_user!
   #before_action :configure_permitted_parameters, if: :devise_controller?
   protect_from_forgery with: :exception
-
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_in) do |user_params|
-      user_params.permit(:username, :email)
-    end
-  end
-
-
-  #def sign_out(*args)
-    #to invalidate session on sign out
-    #current_user.update_attribute(:unique_session_id, "") unless current_user.nil?
-    #current_admin_user.update_attribute(:unique_session_id, "") unless current_admin_user.nil?   
-    #super
-  #end
-
-  #def current_user
-    ##user.where(id: session[:user_id]).first
-  #end
-  #helper_method :current_user  
 end
     
