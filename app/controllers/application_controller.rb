92fba8dd1038f5c3eb89a0f2688fdee3358639f6
AnkitaJaiswal <ankita.jaiswal@quantiguous.com>
2016-03-04 13:35:25 +0530

    fix the sign_out

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,9 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-  before_action :authenticate_user!
+  #before_action :authenticate_user!
   #before_action :configure_permitted_parameters, if: :devise_controller?
-
+  protect_from_forgery with: :exception
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_in) do |user_params|
@@ -11,13 +11,17 @@ class ApplicationController < ActionController::Base
     end
   end
 
-  private 
 
-  def current_user
-    User.where(id: session[:user_id]).first
-  end
-  helper_method :current_user  
-  
-  protect_from_forgery with: :exception
+  #def sign_out(*args)
+    #to invalidate session on sign out
+    #current_user.update_attribute(:unique_session_id, "") unless current_user.nil?
+    #current_admin_user.update_attribute(:unique_session_id, "") unless current_admin_user.nil?   
+    #super
+  #end
+
+  #def current_user
+    ##user.where(id: session[:user_id]).first
+  #end
+  #helper_method :current_user  
 end
     
