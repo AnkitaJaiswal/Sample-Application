2b3ba217d971bdf92a77758263252e48d94b62f0
AnkitaJaiswal <ankita.jaiswal@quantiguous.com>
2016-03-05 15:59:56 +0530

    rspec for user_times controller

--- spec/controllers/user_times_controller_spec.rb
+++ spec/controllers/user_times_controller_spec.rb
@@ -8,23 +8,51 @@ describe UserTimesController do
       get :index
       :user_times 
     end
-    # it "renders the :index view" do
-    #   get :index
-    #   response.should render_template :index
-    # end
   end
 
   describe "GET show" do
     it "assigns all user_time as @user_time" do
-      # user_time = UserTime.create!
       user_time = FactoryGirl.build(:user_time)
-
-      # user_time = UserTime.new
       puts "user_time.inspect :- "
       puts user_time.inspect
-      # get :show, {id: user_time.id}
+    end
+  end
+
+  describe "GET new" do
+    before(:each) do
+    it "assigns a new user_time as @user_time" 
+      get :new
+      assigns(:user_time).should be_a_new(UserTime)
+    end 
+  end
+
+  describe "POST create" do
+    context "with valid params" do
+      it  "creates a new user_time" do
+        expect {
+          params =  FactoryGirl.attributes_for(:user_time)
+        }.to change(UserTime, :count).by(0) 
+      end
+    end
+    context "with invalid params" do
+      it "assigns a newly created but unsaved user_time as @user_time" do
+        expect {
+          params =  FactoryGirl.attributes_for(:user_time)
+        }.to change(UserTime, :count).by(0)
+      end
     end
   end 
 
-end
-  
+  describe "DELETE destroy" do
+   before :each do
+      @user_time = FactoryGirl(:user_time)
+    end
+  end  
+
+  describe "PUT update" do
+    before :each do
+      @user_time = FactoryGirl(:user_time)
+      @user.reload
+    end 
+  end   
+end 
